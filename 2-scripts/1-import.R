6fb2c63fdf07d6c4a34fcb232e93fb76034035a8
remi.vezy <VEZY@users.noreply.github.com>
2020-11-02 17:21:48 +0100

    Update 1-import.R
    Co-Authored-By: burel13 <73695956+burel13@users.noreply.github.com>

--- 2-scripts/1-import.R
+++ 2-scripts/1-import.R
@@ -2,3 +2,8 @@
 # Author: T. Burel & R. Vezy
 # Date: 02/11/2020
 
+
+# Install / load packages -------------------------------------------------
+
+install.packages("remotes")
+remotes::install_github("VEZY/XploRer")
