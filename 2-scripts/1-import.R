33989082882a2e2e6e6eba6ee6133aa547581452
burel13 <73695956+burel13@users.noreply.github.com>
2020-11-13 13:23:32 +0100

    Update 1-import.R

--- 2-scripts/1-import.R
+++ 2-scripts/1-import.R
@@ -26,14 +26,45 @@ names(length_diff) = gsub(".mtg","", basename(mtg_files))
 
 length_diff$A1B1$relative
 length_diff$A1B2$relative
+lengt_diff$A2B3$relative
+length_diff$A3B4$relative
+length_diff$A4B6$relative
+length_diff$A5B7$relative
 
 # Compute relative error mean and sd:
-relative_error = c(length_diff$A1B1$relative,2,3,4)
-mean()
-sd()
+
+relative_error = c(length_diff$A1B1$relative,length_diff$A1B2$relative,
+                   lengt_diff$A2B3$relative,length_diff$A3B4$relative, 
+                   length_diff$A4B6$relative,length_diff$A5B7$relative)
+
+error_vec = c(length_diff$A1B1$relative,length_diff$A1B2$relative,
+             length_diff$A2B3$relative,length_diff$A3B4$relative, 
+             length_diff$A4B6$relative,length_diff$A5B7$relative)
+
+mean(error_vec)
+
+sd(error_vec)
 
 # Interpret boxplot:
-boxplot(relative_error)
+boxplot(error_vec)
 
 # Identify corrections in links between nodes in branches:
-autoplot(MTG_corr)
+
+MTG_corr_A1B1 = read_mtg("1-reconstruction/2-auto_and_manual/A1B1.mtg")
+autoplot(MTG_corr_A1B1)
+
+MTG_corr_A1B2 = read_mtg("1-reconstruction/2-auto_and_manual/A1B2.mtg")
+autoplot(MTG_corr_A1B2)
+
+MTG_corr_A2B3 = read_mtg("1-reconstruction/2-auto_and_manual/A2B3.mtg")
+autoplot(MTG_corr_A2B3)
+
+MTG_corr_A3B4 = read_mtg("1-reconstruction/2-auto_and_manual/A3B4.mtg")
+autoplot(MTG_corr_A3B4)
+
+MTG_corr_A4B6 = read_mtg("1-reconstruction/2-auto_and_manual/A4B6.mtg")
+autoplot(MTG_corr_A4B6)
+
+MTG_corr_A5B7 = read_mtg("1-reconstruction/2-auto_and_manual/A5B7.mtg")
+autoplot(MTG_corr_A5B7)
+
